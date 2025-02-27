f3e666c1c3754be405be7ab97bcd1f0d3762dfac
unknown <nizakatalilaghari7@gmail.com>
2025-02-27 16:46:53 +0500

    Mapping

---
+++ Mapping.py
@@ -0,0 +1,18 @@
+#Dictionary (dict) Stores key-value pairs.
+
+#Set (set) Stores unique values.
+#List (list) Stores ordered collections of values.
+#Tuple (tuple) Stores ordered, immutable collections of values.
+#String (str) Stores a sequence of characters.
+#Boolean (bool) Stores a boolean value (True or False).
+#Integer (int) Stores an integer value.
+#Float (float) Stores a floating-point number.
+
+
+mapping_type: dict={"Name:": "Nizakat Ali" ,"Age:" : 12}
+
+print(type(mapping_type), "Mapping Value:", mapping_type)
+
+mapping_type: dict={"Name:": "Nizakat Ali" ,"Age:" : True}
+
+print(type(mapping_type), "Mapping Value:", mapping_type)
---
+++ set.py
@@ -0,0 +1,11 @@
+set_value : set ={1, 2, 3, 4, 5, 10}
+print(type(set_value), "My Set =", set_value)
+
+
+
+# b. Frozen Set (frozenset)
+# Immutable version of a set.
+
+frozen_set : frozenset=({12,78,00})
+
+print(type(frozen_set), "Frozet Value:", frozen_set)
